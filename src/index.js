68cd934520154a0bac77db6b41efc3e7419dccdb
hmeow <harinithirumalai11@gmail.com>
2022-12-05 11:05:06 -0800

    first react commit change 2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 //import App from './App';
 //import ClassComponent 2 from './ClassComponent 2';
 import reportWebVitals from './reportWebVitals';
-import FormDemo from './FormDemo';
+import FormDemo from './FormDemo1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
